119e0157f9e0b723a3297dddc258577a6f5035b7
Yafet Hailu <yafethailu2026@u.northwestern.edu>
2024-10-06 00:18:53 -0500

    Fixed the print function. It prints the memory addresses now

--- software/apps/gpio/gpio.c
+++ software/apps/gpio/gpio.c
@@ -1,5 +1,6 @@
 
 #include "gpio.h"
+#include "stdio.h"
 
 typedef struct{
   // Step 3:
@@ -13,12 +14,12 @@ typedef struct{
   uint32_t DIRCLR;
   uint32_t LATCH;
   uint32_t DETECTMODE;
-  uint32_t _unused_A[(0x700-0X524)/4 - 1];    // for the unused bits
+  uint32_t _unused_A[(0x700-0x524)/4 - 1];    // for the unused bits
   uint32_t PIN_CNF[32];   //array for pin_cnf values of 0 to 31 (32 values)
 } gpio_reg_t;
 
-  volatile gpio_regs_t* port1 = (gpio_regs_t*)(0x50000000);
-  volatile gpio_regs_t* port2 = (gpio_regs_t*)(0x50000300);
+  volatile gpio_reg_t* port1 = (gpio_reg_t*)(0x50000000);
+  volatile gpio_reg_t* port2 = (gpio_reg_t*)(0x50000300);
 
 
 // Inputs: 
@@ -63,10 +64,16 @@ void gpio_print(void) {
   // Use this function for debugging purposes
   // For example, you could print out struct field addresses
   // You don't otherwise have to write anything here
+    printf("%p\n", &(port1->OUT));
+    printf("Port 1 OUT reg address: %p\n", &(port1->OUT));
+    printf("Port 1 DIR reg address: %p\n", &(port1->DIR));
+    printf("Port 1 PIN_CNF[0] register address: %p\n", &(port1->PIN_CNF));
+
+    printf("Port 2 OUT reg address: %p\n", &(port2->OUT));
+    printf("Port 2 DIR reg address: %p\n", &(port2->DIR));
+    printf("Port 2 PIN_CNF[0] reg address: %p\n", &(port2->PIN_CNF));
+
+
 }
 
-void main ()
-{
-	&(gpio_reg_t->ptr_port1).gpio_print();
-}
 
