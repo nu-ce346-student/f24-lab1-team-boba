de2b3604cd72a7827072a5994b92f0b3baca493a
An Nguyen <annguyen2025@u.northwestern.edu>
2024-10-08 19:05:45 -0500

    check if gpionum < 32 for each func. not working yet

--- software/apps/gpio/gpio.c
+++ software/apps/gpio/gpio.c
@@ -5,6 +5,7 @@
 typedef struct{
   // Step 3:
   // Add register definitions here
+  // uint32_t _unused_A1[(0x504)/4 - 1];    // for the unused bits
 	uint32_t OUT;
   uint32_t OUTSET;
   uint32_t OUTCLR;
@@ -14,13 +15,13 @@ typedef struct{
   uint32_t DIRCLR;
   uint32_t LATCH;
   uint32_t DETECTMODE;
-  uint32_t _unused_A[(0x700-0x524)/4 - 1];    // for the unused bits
+  // uint32_t _unused_A2[(0x700-0x524)/4 - 1];    // for the unused bits
+  uint32_t RESERVED[43];  //between detectmode and pincnf
   uint32_t PIN_CNF[32];   //array for pin_cnf values of 0 to 31 (32 values)
 } gpio_reg_t;
 
-  volatile gpio_reg_t* port1 = (gpio_reg_t*)(0x50000000);
-  volatile gpio_reg_t* port2 = (gpio_reg_t*)(0x50000300);
-
+  volatile gpio_reg_t* port0 = (gpio_reg_t*)(0x50000000 + 0x504);
+  volatile gpio_reg_t* port1 = (gpio_reg_t*)(0x50000300 + 0x504);
 
 // Inputs: 
 //  gpio_num - gpio number 0-31 OR (32 + gpio number)
@@ -29,6 +30,8 @@ void gpio_config(uint8_t gpio_num, gpio_direction_t dir) {
   // Implement me
   // This function should configure the pin as an input/output
   // Hint: Use proper PIN_CNF instead of DIR
+
+  port0->PIN_CNF[gpio_num] = PIN_CNF[gpio_num] & 0xFE | dir;
 }
 
 // Inputs: 
@@ -37,6 +40,7 @@ void gpio_set(uint8_t gpio_num) {
   // Implement me
   // This function should make the pin high
   // It can assume that the pin has already been configured
+  port0->OUT |= 1 << gpio_num; 
 }
 
 // Inputs: 
@@ -45,6 +49,7 @@ void gpio_clear(uint8_t gpio_num) {
   // Implement me
   // This function should make the pin low
   // It can assume that the pin has already been configured
+  port0->OUT &= 0 << gpio_num; 
 }
 
 // Inputs: 
@@ -55,8 +60,7 @@ bool gpio_read(uint8_t gpio_num) {
   // Implement me
   // This function should read the value from the pin
   // It can assume that the pin has already been configured
-
-  return true;
+  return (port0->IN >> gpio_num ) & 0x1;
 }
 
 // prints out some information about the GPIO driver. Can be called from main()
@@ -64,14 +68,14 @@ void gpio_print(void) {
   // Use this function for debugging purposes
   // For example, you could print out struct field addresses
   // You don't otherwise have to write anything here
-    printf("%p\n", &(port1->OUT));
+    printf("%p\n", &(port0->OUT));
+    printf("Port 0 OUT reg address: %p\n", &(port0->OUT));
+    printf("Port 0 DIR reg address: %p\n", &(port0->DIR));
+    printf("Port 0 PIN_CNF[0] register address: %p\n", &(port0->PIN_CNF));
+
     printf("Port 1 OUT reg address: %p\n", &(port1->OUT));
     printf("Port 1 DIR reg address: %p\n", &(port1->DIR));
-    printf("Port 1 PIN_CNF[0] register address: %p\n", &(port1->PIN_CNF));
-
-    printf("Port 2 OUT reg address: %p\n", &(port2->OUT));
-    printf("Port 2 DIR reg address: %p\n", &(port2->DIR));
-    printf("Port 2 PIN_CNF[0] reg address: %p\n", &(port2->PIN_CNF));
+    printf("Port 1 PIN_CNF[0] reg address: %p\n", &(port1->PIN_CNF));
 
 
 }
